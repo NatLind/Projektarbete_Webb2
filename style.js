6a98d69903f7d363c4542078040558fd005fd6a6
Natalie <nl8785@yahoo.se>
2024-12-07 11:25:03 +0100

    fixing the header and adding a timer

--- style.js
+++ style.js
@@ -1,2 +1,25 @@
-$(document).ready(function () 
- 
+$(document).ready(function () {
+    let timer; // Variabel för att lagra setInterval
+    let seconds = 0; // Tid i sekunder
+
+    // Starta timern
+    $('#startBtn').click(function () {
+        clearInterval(timer); // Säkerställ att inga gamla timers körs
+        timer = setInterval(function () {
+            seconds++;
+            $('#timer').text(seconds);
+        }, 1000); // Uppdatera varje sekund
+    });
+
+    // Stoppa timern
+    $('#stopBtn').click(function () {
+        clearInterval(timer);
+    });
+
+    // Återställ timern
+    $('#resetBtn').click(function () {
+        clearInterval(timer);
+        seconds = 0;
+        $('#timer').text(seconds);
+    });
+});
